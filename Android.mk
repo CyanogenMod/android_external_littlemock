da0be1ebc3a6f7dbd434a3a60c5b89a1f96cbc62
Bjorn Bringert <bringert@android.com>
2011-11-25 17:23:38 +0000

    Define SDK version for compiling LittleMock
    Change-Id: I17a6d945ec6be374453f8e17c27ed747c3c4e5b7

--- Android.mk
+++ Android.mk
@@ -18,6 +18,8 @@ include $(CLEAR_VARS)
 # Include all the java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
+LOCAL_SDK_VERSION := 14
+
 # The name of the jar file to create.
 LOCAL_MODULE := littlemock
 
